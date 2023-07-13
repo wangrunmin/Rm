7d5e529e73660f381266d8df13218bbfe3dd8f76
wangrunmin <wrm@dafengtech.com>
2023-07-13 16:49:00 +0800

    添加NET6测试项目

---
+++ TestConsoleApp_NET6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestConsoleApp_NET6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
